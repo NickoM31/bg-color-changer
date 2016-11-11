3d52b8e489170e004a4e24c531d531ccc88af59e
Nicko <nicolasmathieu31@hotmail.com>
2016-11-11 12:42:43 +0100

    Rgb fonctionne sauf alpha

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 (function(){
 	"use strict";
 	var app = {
-		redColor: null,
-		greenColor : null,
-		blueColor : null,
-		alphaColor : null,
+		rc:null,
+		gc:null,
+		bc:null,
+		ac:null,
 		init:function(){
 			app.listeners();
-			// let's go
 		},
 		listeners:function(){
-			$('#red').on('change input', function(){
-				console.log("progress");			
-			});
-			$('#green').on('change input', function(){
-				console.log("progress");	
-			});
-			$('#blue').on('change input', function(){
-				console.log("progress");
-			});
-			$('#alpha').on('change input', function(){
-				console.log("progress");
-			});
+			$('#red').on('change input', app.redColor);
+			$('#green').on('change input', app.greenColor);
+			$('#blue').on('change input', app.blueColor);
+			$('#alpha').on('change input', app.alphaColor);
 		},
-		
-		
+		redColor : function(){
+			app.rgb();
+		},
+		greenColor : function(){
+			app.rgb();
+		},
+		blueColor : function(){
+			app.rgb();
+		},
+		alphaColor : function(){
+			app.rgb();
+		},
+	// 	//Faire une fonction qui additionne les couleurs
+	rgb :function(){
+		var rc = $('#red').val();
+		var gc = $('#green').val();
+		var bc = $('#blue').val();
+		var ac = $('#alpha').val();
+		$('body').css('background-color', "rgb("+rc+","+gc+","+bc+")");
+		$('body').css('background-color', "rgba("+rc+","+gc+","+bc+","+ac+")");
+	},
 
-	};
+};
 
 
-	$(document).ready(function(){
-		app.init();
-	});
+$(document).ready(function(){
+	app.init();
+});
 })();
